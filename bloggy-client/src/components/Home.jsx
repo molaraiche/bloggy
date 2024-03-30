665ce29f2823cd13988c3274781bd3e7bffb87a9
molaraiche <laraichemohamed@gmail.com>
2024-03-30 15:29:03 +0000

    updates

--- bloggy-client/src/components/Home.jsx
+++ bloggy-client/src/components/Home.jsx
@@ -1,16 +1,18 @@
-import React from "react";
-import Sliders from "./Sliders";
-import { Link } from "react-router-dom";
+import React from 'react';
+import Sliders from './Sliders';
+import { Link } from 'react-router-dom';
 const Home = ({ blogs, load }) => {
   return (
     <div>
       {blogs.length === 0 ? (
-        <p className="empty">Please add a blog First !</p>
+        <p className='empty'>
+          Please add a blog First Or Wait for blogs to fetch (free server)
+        </p>
       ) : (
         <>
           <Sliders blogs={blogs} load={load} />
-          <section className="discoverHolder">
-            <Link className="discoverAll" to="/blogs">
+          <section className='discoverHolder'>
+            <Link className='discoverAll' to='/blogs'>
               Discover All
             </Link>
           </section>
